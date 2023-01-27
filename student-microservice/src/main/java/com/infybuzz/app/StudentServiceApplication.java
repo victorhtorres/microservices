9e72414e2e6d28bc5220a5ecdd38cc6a3a6bb154
vtorres <victorhugo.torres@blackboard.com>
2023-01-26 21:27:22 -0500

    Configured Webclient for using address dependency on student microservices, but returning null for address object yet

--- student-microservice/src/main/java/com/infybuzz/app/StudentServiceApplication.java
+++ student-microservice/src/main/java/com/infybuzz/app/StudentServiceApplication.java
@@ -1,19 +1,33 @@
 package com.infybuzz.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @ComponentScan({"com.infybuzz.controller", "com.infybuzz.service"})
 @EntityScan("com.infybuzz.entity")
 @EnableJpaRepositories("com.infybuzz.repository")
 public class StudentServiceApplication {
+	
+	@Value("${address.service.url}")
+	private String addressServiceUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentServiceApplication.class, args);
+	}
+	
+	@Bean
+	public WebClient webClient () {
+		WebClient webClient =WebClient.builder()
+				.baseUrl(addressServiceUrl).build();
+		
+		return webClient;
 	}
 
 }
--- student-microservice/src/main/java/com/infybuzz/service/StudentService.java
+++ student-microservice/src/main/java/com/infybuzz/service/StudentService.java
@@ -2,6 +2,7 @@ package com.infybuzz.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.infybuzz.entity.Student;
 import com.infybuzz.repository.StudentRepository;
@@ -13,6 +14,9 @@ public class StudentService {
 
 	@Autowired
 	StudentRepository studentRepository;
+	
+	@Autowired
+	WebClient webClient;
 
 	public StudentResponse createStudent(CreateStudentRequest createStudentRequest) {
 
