c6fe8f82a9be50ff70aefa739a4f923a08c20ab7
vtorres <victorhugo.torres@blackboard.com>
2023-01-27 20:39:04 -0500

    Configured OpenFeign on student microservice

--- student-microservice/src/main/java/com/infybuzz/app/StudentServiceApplication.java
+++ student-microservice/src/main/java/com/infybuzz/app/StudentServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,6 +14,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @ComponentScan({"com.infybuzz.controller", "com.infybuzz.service"})
 @EntityScan("com.infybuzz.entity")
 @EnableJpaRepositories("com.infybuzz.repository")
+@EnableFeignClients("com.infybuzz.feignclients")
 public class StudentServiceApplication {
 	
 	@Value("${address.service.url}")
