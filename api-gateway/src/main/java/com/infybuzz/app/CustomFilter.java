f2d0c216ac29df5894cfee0d3ea2cec4f0261ef8
vtorres <victorhugo.torres@blackboard.com>
2023-02-01 21:18:25 -0500

    Added post-filter example to all microservices by api gateway

--- api-gateway/src/main/java/com/infybuzz/app/CustomFilter.java
+++ api-gateway/src/main/java/com/infybuzz/app/CustomFilter.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
@@ -20,9 +21,17 @@ public class CustomFilter implements GlobalFilter {
 		
 		ServerHttpRequest request = exchange.getRequest();
 		
+		if (request.getURI().toString().contains("/api/student/")) {
+			
+		}
+		
 		logger.info("Authorization = " + request.getHeaders().getFirst("Authorization"));
 		
-		return chain.filter(exchange);
+		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+			ServerHttpResponse response = exchange.getResponse();
+			
+			logger.info("Post Filter = " + response.getStatusCode());
+		}));
 	}
 	
 }
