f7de87ceaa432ffe51c8b7ab757af7df4d339bee
vtorres <victorhugo.torres@blackboard.com>
2023-02-07 08:25:23 -0500

    Configure Spring Cloud Sleuth to API Gateway, Student and Address microservices

--- student-microservice/src/main/java/com/infybuzz/service/StudentService.java
+++ student-microservice/src/main/java/com/infybuzz/service/StudentService.java
@@ -1,5 +1,7 @@
 package com.infybuzz.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,13 +10,12 @@ import com.infybuzz.entity.Student;
 import com.infybuzz.feignclients.AddressFeignClient;
 import com.infybuzz.repository.StudentRepository;
 import com.infybuzz.request.CreateStudentRequest;
-import com.infybuzz.response.AddressResponse;
 import com.infybuzz.response.StudentResponse;
-
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @Service
 public class StudentService {
+	
+	Logger logger = LoggerFactory.getLogger(StudentService.class);
 
 	@Autowired
 	StudentRepository studentRepository;
@@ -48,6 +49,9 @@ public class StudentService {
 	}
 	
 	public StudentResponse getById (long id) {
+		
+		logger.info("Inside Student getById");
+		
 		Student student = studentRepository.findById(id).get();
 		StudentResponse studentResponse = new StudentResponse(student);
 		
